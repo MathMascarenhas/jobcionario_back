c8da42d65d8a3d7a460ac3face5c54db9fc80ffb
Matheus Mascarenhas <mathmasca@gmail.com>
2022-10-04 17:48:09 +0100

    #

--- server.js
+++ server.js
@@ -8,5 +8,17 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+server.use(jsonServer.bodyParse);
+server.post('/words', (req, res) => {
+    if (req.method === 'POST') {
+      let newWord = req.body;
+      data.push({
+        "word": newWord.word,
+        "translation": newWord.translation,
+        "definition": newWord.definition});
+      word = data.find(data => {return data.word == newWord.word});
+      return word;
+    }
+});
 
 server.listen(port);
